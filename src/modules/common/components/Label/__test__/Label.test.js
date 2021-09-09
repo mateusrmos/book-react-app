bc7b1adfb8b953bc95c65e43aecd99725c1dd97a
mateusrmos <mateus-bh@hotmail.com>
2021-09-08 22:31:03 -0300

    Removed unused function

--- src/modules/common/components/Label/__test__/Label.test.js
+++ src/modules/common/components/Label/__test__/Label.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Label from "../Label";
 let container = null;
 let labelText = "LabelText";
